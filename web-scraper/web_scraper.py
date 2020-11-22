256c78f8826da82b78d92205ad5e146002d15cfd
Sami Cheema <53884212+samitcheema@users.noreply.github.com>
2020-11-21 22:39:41 -0500

    time function added

--- web-scraper/web_scraper.py
+++ web-scraper/web_scraper.py
@@ -1,5 +1,7 @@
 # simple web scraper via xpath
 from selenium import webdriver
+import time 
+
 
 url = 'https://www.youtube.com/channel/UCFTM1FGjZSkoSPDZgtbp7hA'
 path_to_chromedriver = './chromedriver'  # change as necessary
@@ -7,4 +9,7 @@ path_to_chromedriver = './chromedriver'  # change as necessary
 browser = webdriver.Chrome(path_to_chromedriver)
 browser.get(url)
 
+time.sleep(3)
+
 browser.find_element_by_xpath('//*[@id="thumbnail"]').click()
+browser.quit()
